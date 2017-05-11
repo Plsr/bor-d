d4f982b552a44a8da2873279fa87d697287d7327
Plsr <christianpoplawski@gmail.com>
2017-05-11 22:16:20 +0200

    Added some article validations

--- app/models/article.js
+++ app/models/article.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const Nodal = require('nodal');
+const validUrl = require('valid-url');
 
 class Article extends Nodal.Model {}
 
 Article.setDatabase(Nodal.require('db/main.js'));
 Article.setSchema(Nodal.my.Schema.models.Article);
+
+Article.validates("url", "must be a valid url", v => v && validUrl.isWebUri(v));
+Article.validates("name", "must not be empty", v => v && v);
+Article.validates("name", "must not be longer than 140 character", v => v && v.length <= 140);
 
 module.exports = Article;
